a8274e76ba0559ae75485d9f340ad9696debd2f9
Joinemm <joonas@rautiola.co>
2022-12-06 20:22:56 +0200

    :hammer: Update template

--- src/template.rs
+++ src/template.rs
@@ -9,27 +9,27 @@ fn solve_part1(input: &str) -> u32 {}
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::get_input;
+    use crate::test_utils::*;
     use test_case::test_case;
 
     const EXAMPLE_INPUT: &str = "";
 
-    #[test_case(EXAMPLE_INPUT => 157)]
+    #[test_case(EXAMPLE_INPUT => 000)]
     fn part1_example(input: &str) -> u32 {
         solve_part1(input)
     }
 
-    // #[test_case(EXAMPLE_INPUT => 70)]
+    // #[test_case(EXAMPLE_INPUT => 000)]
     // fn part2_example(input: &str) -> u32 {
     //     solve_part2(input)
     // }
 
-    // #[test_case(&get_input("dayX") => 8139)]
+    // #[test_case(&get_input("dayX") => 000)]
     // fn part1_real(input: &str) -> u32 {
     //     solve_part1(input)
     // }
 
-    // #[test_case(&get_input("dayX") => 2668)]
+    // #[test_case(&get_input("dayX") => 000)]
     // fn part2_real(input: &str) -> u32 {
     //     solve_part2(input)
     // }
