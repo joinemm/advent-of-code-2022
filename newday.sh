86f8da53f93de5862c051ab7038051b7add1f823
Joinemm <joonas@rautiola.co>
2022-12-15 22:41:47 +0200

    :hammer: Update templating script to reflect new path

--- newday.sh
+++ newday.sh
@@ -2,7 +2,7 @@
 
 DAY=$1
 export DAY
-cat src/template.rs | envsubst > src/day${DAY}.rs
+cat src/template.rs | envsubst > src/days/day${DAY}.rs
 touch "input/2022/day${DAY}_example.txt"
 cargo aoc input -d ${DAY}
 echo "Good luck with day ${DAY}!"
