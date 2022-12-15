8216344d26af9c2a1075d2e43ad2e0e272230772
Joinemm <joonas@rautiola.co>
2022-12-15 22:33:00 +0200

    :truck: Move days into an autoimporting module folder

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,7 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
-pub mod day1;
-pub mod day10;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
+pub mod days;
 
 #[cfg(test)]
 mod test_utils {
